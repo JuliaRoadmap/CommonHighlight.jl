195bc69712dbe2e849044b1877743e61c3503a17
Rratic <Rratic@163.com>
2022-07-07 12:48:25 +0800

    fix:shell rules

--- src/default/shell.jl
+++ src/default/shell.jl
@@ -4,9 +4,10 @@ function highlight_lines(::Union{Val{:shell}, Val{:sh}}, content::AbstractString
 	sizehint!(vec, length(lines))
 	meta=Dict(:record_linestart => "")
 	vec=useruleset(RuleSet([
-		dollar_rule=LineStartRule(;pattern="\$ ", patlength = 2,offset=-1),
-		long_rule=LineStartRule(;pattern=r"^[a-zA-Z0-9_-]*(>|#|~) ", offset=-1),
-		plain_rule=PlainAllRule()
+		RecordedLineStartRule(),
+		LineStartRule(;pattern="\$ ", patlength = 2,offset=-1),
+		LineStartRule(;pattern=r"^[a-zA-Z0-9_-]*(>|#|~) ", offset=-1),
+		PlainAllRule()
 	]), lines, nothing, meta)
 	return HighlightLines{Tuple}(vec)
 end
--- src/rules.jl
+++ src/rules.jl
@@ -22,7 +22,7 @@ function canuserule end
 """
 This rule is used to recognize patterns at the start of a line.
 - `pattern::Union{AbstractString, Regex}`
-- `patlength::Int = 0` for strings, this should usually be set to `length(pattern)`, and for regex, this means the last index of found pattern must not be less than `patlength`
+- `patlength::Int = 0` for strings, this should usually be set to `sizeof(pattern)`, and for regex, this means the last index of found pattern must not be less than `patlength`
 - `offset::Integer = 0` set the offset after pattern found
 - `hl_type::Symbol = :shell`
 - `record::Bool = false` whether to record for `RecordedLineStartRule`
@@ -34,7 +34,6 @@ Base.@kwdef struct LineStartRule <: CommonHighlightRule
 	hl_type::Symbol = :shell
 	record::Bool = false
 end
-
 canuserule(::LineStartRule, ::AbstractString, i::Int, stack, meta)= i==1
 function userule(r::LineStartRule, str::AbstractString, ::Int, stack, meta)
 	if isa(r.pattern, Regex)
@@ -55,6 +54,22 @@ function userule(r::LineStartRule, str::AbstractString, ::Int, stack, meta)
 	end
 end
 
+"""
+This rule checks `meta[:record_linestart]`
+"""
 struct RecordedLineStartRule <: CommonHighlightRule end
+canuserule(::RecordedLineStartRule, ::AbstractString, i::Int, stack, meta)= i==1
+function userule(::RecordedLineStartRule, str::AbstractString, ::Int, stack, meta)
+	record=meta[:record_linestart]
+	if startswith(str, record)
+		return (sizeof(str)+1, :shell => record)
+	else
+		return nothing
+	end
+end
 
 struct PlainAllRule <: CommonHighlightRule end
+canuserule(::PlainAllRule, ::AbstractString, i::Int, stack, meta)= true
+function userule(::RecordedLineStartRule, str::AbstractString, i::Int, stack, meta)
+	return (sizeof(str)+1, :plain => str[i:end])
+end
